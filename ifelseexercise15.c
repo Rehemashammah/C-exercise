5666528f1995714d5a5115a46fa1da3cea731983
Rehemashammah <rehemashammah@gmail.com>
2023-08-15 00:00:34 +0300

    Exercise 15

---
+++ ifelseexercise15.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+//Program to enter basic salary and calculate gross salary of an employee
+float basic, gross, da, hra;
+printf("Enter the basic salary:");
+scanf("%f", &basic);
+//Calculate D.A and H.R.A according to specifie condition
+if (basic <= 10000)
+{
+da=basic*0.8;
+hra=basic*0.2;
+}
+else if ( basic <= 20000)
+{
+da = basic * 0.9;
+hra = basic * 0.25;
+}
+else
+{
+da = basic * 0.95;
+hra = basic * 0.3;
+}
+//Calculate gross salary
+gross = basic +hra +da;
+printf("GROSS SALARY OF EMPLOYEE = %.2f", gross);
+return 0;
+}
